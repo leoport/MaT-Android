44b2f5db1a96106aedfdb699f667f14bec49f19d
leoport <leoport@gmail.com>
2015-07-12 09:45:43 +0800

    Add CalendarActivity

--- app/src/main/java/org/leopub/mat/controller/CalendarActivity.java
+++ app/src/main/java/org/leopub/mat/controller/CalendarActivity.java
@@ -21,25 +21,79 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class CalendarActivity extends Activity {
+    final private float mContentOriginX = 100.0f;
+    final private float mContentOriginY = 100.0f;
+    private float mContentWidth;
+    private float mContentHeight;
+    final private int N_DAY_PER_WEEK = 7;
+    final private int N_ROW = 8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(new CalendarView(this));
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     private class CalendarView extends View {
+        private Paint mPaint;
+        private Paint mDayHeaderText;
+        private Paint mDayHeaderBg;
+        final private float DAY_HEADER_FONT_SIZE = 30.0f;
+        final private float DAY_HEADER_PADDING = 10.0f;
         public CalendarView(Context context) {
             super(context);
+            mPaint = new Paint();
+            mPaint.setColor(0xff808080);
+            mDayHeaderText = new Paint();
+            mDayHeaderText.setColor(0xffffffff);
+            mDayHeaderText.setTextSize(DAY_HEADER_FONT_SIZE);
+            mDayHeaderBg = new Paint();
+            mDayHeaderBg.setColor(0xff80c0ff);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
-            Paint paint = new Paint();
-            canvas.drawLine(100, 100, getWidth(), getHeight(), paint);
+            mContentWidth = getWidth();
+            mContentHeight = getHeight();
+            float itemWidth = (mContentWidth - mContentOriginX) / N_DAY_PER_WEEK;
+            float itemHeight = (mContentHeight - mContentOriginY) / N_ROW;
+
+            mPaint.setTextSize(30);
+            canvas.drawRect(mContentOriginX, 0, mContentWidth, mContentOriginY, mDayHeaderBg);
+            canvas.drawText("19周", 0, 100, mDayHeaderText);
+            float headerStartY = DAY_HEADER_FONT_SIZE + DAY_HEADER_PADDING;
+            for (int i = 0; i < N_DAY_PER_WEEK; i++) {
+                if (i == 0) {
+                    mPaint.setStrokeWidth(5);
+                } else {
+                    mPaint.setStrokeWidth(3);
+                }
+                float x = mContentOriginX + i * itemWidth;
+                canvas.drawLine(x, 0, x, mContentHeight, mPaint);
+                canvas.drawText(String.valueOf(i + 1), x + DAY_HEADER_PADDING, headerStartY, mDayHeaderText);
+            }
+            for (int i = 0; i < N_ROW; i++) {
+                if (i == 0) {
+                    mPaint.setStrokeWidth(3);
+                } else {
+                    mPaint.setStrokeWidth(1);
+                }
+                float y = mContentOriginY + i * itemHeight;
+                canvas.drawLine(0, y, mContentWidth, y, mPaint);
+            }
         }
     }
 }
